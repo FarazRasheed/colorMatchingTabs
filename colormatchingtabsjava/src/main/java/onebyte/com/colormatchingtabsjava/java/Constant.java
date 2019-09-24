d8b49517770e3aecc1d2d2c3799c4d60c0448069
Faraz Rasheed <faraz.rasheed@onebyte.biz>
2019-09-24 15:52:29 +0500

    clean repo

--- colormatchingtabsjava/src/main/java/onebyte/com/colormatchingtabsjava/java/Constant.java
+++
@@ -1,7 +0,0 @@
-package onebyte.com.colortabs;
-
-public class Constant {
-       public static long ANIMATION_DURATION = 200L;
-    public static long ANIMATION_TEXT_APPEARANCE_DURATION = 100L;
-
-}
